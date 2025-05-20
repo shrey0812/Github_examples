4b30ab6c8e2dec89c275c530cbce3aa958796b8b
Shrey Gupta -2 <guptashrey59@gmail.com>
2025-05-20 05:57:45 +0000

    sdk

--- github-sdks/javascript/main.js
+++ github-sdks/javascript/main.js
@@ -4,9 +4,9 @@ const octokit = new Octokit({
   auth: process.env.GH_TOKEN
 })
 
-await octokit.request('GET /repos/{owner}/{repo}/git/matching-refs/{ref}', {
+ octokit.request('GET /repos/{owner}/{repo}/git/matching-refs/{ref}', {
   owner: 'shrey-2-gupta',
-  repo: 'Github_exapmles',
+  repo: 'Github_examples',
   ref: 'refs/heads/sdkjs',
   headers: {
     'X-GitHub-Api-Version': '2022-11-28'
